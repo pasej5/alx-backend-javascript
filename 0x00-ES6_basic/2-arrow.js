b6f844f95ab574117a9816b01c68f4c46bdcda6f
Jealous Matsikachando <pasej5@gmail.com>
2024-01-03 14:54:46 +0200

    using arrow function on es6

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,11 +1,9 @@
-/*  ES6’s arrow syntax of the function add */
-export default class NeighborhoodsList {
-  constructor() {
-    this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
-  }
+export default function getNeighborhoodsList() {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
-  addNeighborhood = (newNeighborhood) => {
-    this.sanFranciscoNeighborhoods.push(newNeighborhood);
-    return this.sanFranciscoNeighborhoods;
+  const self = this;
+  this.addNeighborhood = (newNeighborhood) => {
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
   };
 }
