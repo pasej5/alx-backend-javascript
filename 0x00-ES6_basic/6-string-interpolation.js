652e5e55b07cc7f54e4c3598370223c656d7c4af
Jealous Matsikachando <pasej5@gmail.com>
2024-01-03 16:12:24 +0200

    Take advantage of template literals

--- 0x00-ES6_basic/6-string-interpolation.js
+++ 0x00-ES6_basic/6-string-interpolation.js
@@ -7,6 +7,10 @@ export default function getSanFranciscoDescription() {
   };
 
   return `As of ${year}, it was the seventh-highest income county in the United States, 
-      with a per capita personal income of ${budget.income}. As of ${year}, San Francisco 
-      proper had a GDP of ${budget.gdp}, and a GDP per capita of ${budget.capita}.`;
+      with a per capita personal income of ${budget.income}. As of ${
+    year - 2
+  }, San Francisco 
+      proper had a GDP of ${budget.gdp}, and a GDP per capita of ${
+    budget.capita
+  }.`;
 }
