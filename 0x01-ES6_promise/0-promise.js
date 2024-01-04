991d42c870cda6dbd6e793b96202c5ae3f726dd2
Jealous Matsikachando <pasej5@gmail.com>
2024-01-04 03:44:38 +0200

    Keep every promise you make and only make promises you can keep

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,11 +1,5 @@
 /* simulating an asynchronous API call that retuens a promise */
 
-function getResponseFromAPI() {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve('Response from the API');
-    }, 2000);
-  });
+export default function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {});
 }
-
-export default getResponseFromAPI;
