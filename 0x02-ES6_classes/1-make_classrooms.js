63f45914fb3a6f81d76a5f756f06d5f22269e09f
Jealous Matsikachando <pasej5@gmail.com>
2024-01-05 09:43:13 +0200

    Let's make some classrooms

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,11 @@
+import ClassRoom from './0-classroom.js';
+
+
+function initializeRooms() {
+	const array = [
+		new ClassRoom(19),
+		new ClassRoom(20),
+		new ClassRoom(34),
+	];
+	return (array);
+}
