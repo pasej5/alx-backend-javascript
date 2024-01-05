c49570a657e106c9b085f98bcc22256e5c10e12d
Jealous Matsikachando <pasej5@gmail.com>
2024-01-05 10:47:51 +0200

    A Course, Getters, and Setters

--- 0x02-ES6_classes/2-main.js
+++ 0x02-ES6_classes/2-main.js
@@ -1,21 +1,5 @@
-import HolbertonCourse from "./2-hbtn_course.js";
+import getNeighborhoodsList from './2-arrow.js';
 
-const c1 = new HolbertonCourse("ES6", 1, ["Bob", "Jane"])
-console.log(c1.name);
-c1.name = "Python 101";
-console.log(c1);
-
-try {
-    c1.name = 12;
-} 
-catch(err) {
-    console.log(err);
-}
-
-try {
-    const c2 = new HolbertonCourse("ES6", "1", ["Bob", "Jane"]);
-}
-catch(err) {
-    console.log(err);
-}
-
+const neighborhoodsList = new getNeighborhoodsList();
+const res = neighborhoodsList.addNeighborhood('Noe Valley');
+console.log(res);
