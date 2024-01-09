989ca06360e542f9e32771a0d377b4ab5dd9ddf0
Jealous Matsikachando <pasej5@gmail.com>
2024-01-09 04:19:42 +0200

    basic lists of objects

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,5 @@
+export default function getListStudents() {
+  return [{ studentName: 'Guillaume', id: 1, state: 'San Francisco' }];
+  return [{ studentName: 'James', id: 2, state: 'Columbia' }];
+  return [{ studentName: 'Serena', id: 5, state: 'San Francisco' }];
+}
