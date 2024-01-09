a2234d1625e009dae698b22dd039cfbb61ca316f
Jealous Matsikachando <pasej5@gmail.com>
2024-01-09 15:28:00 +0200

    return new buffer

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,6 +1,6 @@
 export default function createInt8TypedArray(length, position, value) {
   if (position < 0 || position >= length) {
-    throw Error('Position outside rang(e');
+    throw Error('Position outside range');
   }
   const newBuffer = new ArrayBuffer(length);
   const int8 = new Int8Array(newBuffer, 0, length);
