8340e4e0213046f15240f38e00493258e5b27ecd
Jealous Matsikachando <pasej5@gmail.com>
2024-01-10 00:24:24 +0200

    Map data structure

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,12 @@
+const groceriesList = () => {
+  const groceries = new Map(
+    ['Apples, 10'],
+    ['Tomatoes, 10'],
+    ['Pasta, 10'],
+    ['Rice, 10'],
+    ['Banana, 10'],
+  );
+  return groceries;
+};
+
+export default groceriesList;
