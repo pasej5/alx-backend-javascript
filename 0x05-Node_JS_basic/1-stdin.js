af96d9d50452d1189c4be34607af82e4efa62368
Jealous Matsikachando <pasej5@gmail.com>
2024-02-28 01:55:36 +0200

    Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -10,9 +10,9 @@ const { stdout, stdin } = process;
 stdout.write('Welcome to Holberton School, what is your name?\n');
 
 stdin.setEncoding('utf-8');
-stdin.on('readable', () => {
-  const name = stdin.read();
-  stdout.write(`Your name is: ${name}`);
+stdin.on('data', (data) => {
+  const name = data.trim();
+  stdout.write(`Your name is: ${name}\n`);
   process.exit();
 });
 
