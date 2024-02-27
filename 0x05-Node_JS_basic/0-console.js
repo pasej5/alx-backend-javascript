c8d0229757e2605447230955050671b6cfd778ed
Jealous Matsikachando <pasej5@gmail.com>
2024-02-27 20:33:57 +0200

    Executing basic javascript with Node JS

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,9 @@
+/**
+ * function that prints in STDOUT the string argument
+ * @param {string} string - message to print
+ */
+function displayMessage(message) {
+  console.log(message);
+}
+
+module.exports = displayMessage;
